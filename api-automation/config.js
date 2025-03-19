5a6a49928d6d379925aa35913c68a67b2f3aac8e
F1re8oy <ronaldu03@gmail.com>
2025-03-19 10:32:17 +0200

    add

---
+++ api-automation/config.js
@@ -0,0 +1,18 @@
+export const config = {
+    STG: {
+        host: 'https://gorest.co.in/public/v2',
+        token: 'ecccb9ce4fb15faec3dc36b87ff8ffe282bf16133f592dc4b064f8dc39ebcc44',
+        username: 'Tenali Ramakrishna',
+        gender: 'male',
+        status: 'active'
+    },
+    PROD: {
+        host: 'https://gorest.co.in/public/v2',
+        token: 'ecccb9ce4fb15faec3dc36b87ff8ffe282bf16133f592dc4b064f8dc39ebcc44',
+        username: 'Tenali Ramakrishna',
+        gender: 'male',
+        status: 'active'
+    }
+}
+
+global.executionVariables = {}
