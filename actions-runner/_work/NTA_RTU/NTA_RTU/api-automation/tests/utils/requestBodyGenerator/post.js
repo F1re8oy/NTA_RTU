77a727ba943366d5669d4ee184523998879d22c4
F1re8oy <ronaldu03@gmail.com>
2025-03-21 10:46:53 +0200

    all

--- actions-runner/_work/NTA_RTU/NTA_RTU/api-automation/tests/utils/requestBodyGenerator/post.js
+++
@@ -1,8 +0,0 @@
-import {faker} from '@faker-js/faker';
-
-export async function getCreatePostRequestBody() {
-    return {
-        title: faker.book.title(),
-        body: faker.lorem.sentence()
-    }
-}
--- actions-runner/_work/NTA_RTU/NTA_RTU/api-automation/tests/utils/requestBodyGenerator/todo.js
+++
@@ -1,8 +0,0 @@
-import {faker} from '@faker-js/faker';
-
-export async function getCreateTodoRequestBody(status) {
-    return {
-        title: faker.book.title(), 
-        status: status
-    }
-}
